2b4e61e1420538751a51656c6ecd02a4a02e8c10
morrillo <gustavo.orrillo@moldeo.coop>
2016-07-08 16:37:25 -0300

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Accounting - Partial Refund Conciliation',
+    'category': 'Sales',
+    'version': '0.1',
+    'depends': ['base','account'],
+    'data': [
+	#'security/security.xml',
+	#'security/ir.model.access.csv',
+	#'portal_view.xml',
+	#'account_view.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+}
