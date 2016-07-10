af8b7890688dd1e8e5892aaf0ce44f1a7658a6ad
morrillo <gustavo.orrillo@moldeo.coop>
2016-07-10 13:08:35 -0300

    Added line ids to partial reconciliation

--- wizard/wizard_model.py
+++ wizard/wizard_model.py
@@ -24,10 +24,10 @@ class refund_add_invoice(models.TransientModel):
         def confirm_line(self):
 		context = self.env.context
 		refund = self.env['account.invoice'].browse(context['active_id'])
-		debit_account = refund.partner_id.property_account_receivable
+		credit_account = refund.partner_id.property_account_receivable
 		if refund.invoice_line:
 			line = refund.invoice_line[0]
-			credit_account = line.account_id
+			debit_account = line.account_id
 		else:
                         raise exceptions.ValidationError('No pued determinar cuenta contable')
 		if not refund:
@@ -41,27 +41,35 @@ class refund_add_invoice(models.TransientModel):
 		move_id = self.env['account.move'].create(vals_move)
 		if not move_id:
                         raise exceptions.ValidationError('No pudo crearse la cabecera del asiento contable')
+		rec_ids = []
 		for line in self.lines:
-			vals_debit = {
-				'name': 'DEBIT - Partial refund conciliation' + str(move_id.id),
-				'move_id': move_id.id,
-				'partner_id': refund.partner_id.id,
-				'account_id': debit_account.id,
-				'debit': line.amount,
-				'credit': 0
-				}
-			debit_move_id = self.env['account.move.line'].create(vals_debit)
-			vals_credit = {
-				'name': 'CREDIT - Partial refund conciliation' + str(move_id.id),
-				'move_id': move_id.id,
-				'partner_id': refund.partner_id.id,
-				'account_id': credit_account.id,
-				'debit': 0,
-				'credit': line.amount
-				}
-			credit_move_id = self.env['account.move.line'].create(vals_credit)
+			if line.amount > 0:	
+				invoice = line.invoice_id
+				for invoice_line in invoice.move_lines:
+					if invoice_line.debit > 0:
+						invoice_line_id = invoice_line.id
+				vals_debit = {
+					'name': 'DEBIT - Partial refund conciliation' + str(move_id.id),
+					'move_id': move_id.id,
+					'partner_id': refund.partner_id.id,
+					'account_id': debit_account.id,
+					'debit': line.amount,
+					'credit': 0
+					}
+				debit_move_id = self.env['account.move.line'].create(vals_debit)
+				vals_credit = {
+					'name': 'CREDIT - Partial refund conciliation' + str(move_id.id),
+					'move_id': move_id.id,
+					'partner_id': refund.partner_id.id,
+					'account_id': credit_account.id,
+					'debit': 0,
+					'credit': line.amount
+					}
+				credit_move_id = self.env['account.move.line'].create(vals_credit)
+				rec_ids.append([line_line_id,credit_move_id.id])	
 		if move_id and debit_move_id and credit_move_id:
 			move_id.post()
+		import pdb;pdb.set_trace()
 		# reconcile = move_line_pool.reconcile_partial(cr, uid, rec_ids, writeoff_acc_id=voucher.writeoff_acc_id.id, writeoff_period_id=voucher.period_id.id, writeoff_journal_id=voucher.journal_id.id)
                 return None
 
