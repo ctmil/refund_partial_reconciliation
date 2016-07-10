62a2eb2adc0cfa62415c13e10457ed00c421ba45
morrillo <gustavo.orrillo@moldeo.coop>
2016-07-10 10:20:56 -0300

    First shot at creating move

--- wizard/wizard_model.py
+++ wizard/wizard_model.py
@@ -7,6 +7,8 @@ import openerp.addons.decimal_precision as dp
 from datetime import date
 import logging
 import ast
+from openerp.exceptions import ValidationError
+
 #Get the logger
 _logger = logging.getLogger(__name__)
 
@@ -14,13 +16,23 @@ class refund_add_invoice(models.TransientModel):
         _name = 'refund.add.invoice'
 
 	name = fields.Char(string='Name')
-	journal_id = fields.Many2one('account.journal',string='Diario',domain=[('type','=','general')])
+	journal_id = fields.Many2one('account.journal',string='Diario',domain=[('type','=','general')],required=True)
 	lines = fields.One2many(comodel_name='refund.add.invoice.line',inverse_name='header_id')
 	refund_id = fields.Many2one('account.invoice')
 
         @api.multi
         def confirm_line(self):
-                import pdb;pdb.set_trace()
+		import pdb;pdb.set_trace()
+		vals_move = {
+			'journal_id': self.journal_id.id,
+			'date': str(date.today()),
+			'company_id': 1,
+			'partner_id':
+			}
+		move_id = self.env['account.move'].create(vals_move)
+		if not move_id:
+                        raise exceptions.ValidationError('No pudo crearse la cabecera del asiento contable')
+
                 return None
 
 class refund_add_invoice_line(models.TransientModel):
