13ea3d0700625d403d3d741fee592d154152c10a
morrillo <gustavo.orrillo@moldeo.coop>
2016-07-10 11:05:04 -0300

    First shot at creating move lines

--- wizard/wizard_model.py
+++ wizard/wizard_model.py
@@ -22,16 +22,45 @@ class refund_add_invoice(models.TransientModel):
 
         @api.multi
         def confirm_line(self):
-		import pdb;pdb.set_trace()
+		context = self.env.context
+		refund = self.env['account.invoice'].browse(context['active_id'])
+		debit_account = refund.partner_id.property_account_receivable
+		if refund.invoice_line:
+			line = refund.invoice_line[0]
+			credit_account = line.account_id
+		else:
+                        raise exceptions.ValidationError('No pued determinar cuenta contable')
+		if not refund:
+                        raise exceptions.ValidationError('No hay NC disponible')
 		vals_move = {
 			'journal_id': self.journal_id.id,
 			'date': str(date.today()),
-			'company_id': 1,
+			'company_id': refund.company_id.id,
+			'partner_id': refund.partner_id.id,
 			}
 		move_id = self.env['account.move'].create(vals_move)
 		if not move_id:
                         raise exceptions.ValidationError('No pudo crearse la cabecera del asiento contable')
-
+		for line in self.lines:
+			import pdb;pdb.set_trace()
+			vals_debit = {
+				'move_id': move_id.id,
+				'partner_id': refund.partner_id.id,
+				'account_id': debit_account.id,
+				'currency_id': refund.currency_id.id,
+				'debit': line.amount,
+				'credit': 0
+				}
+			debit_move_id = self.env['account.move.line'].create(vals_debit)
+			vals_credit = {
+				'move_id': move_id.id,
+				'partner_id': refund.partner_id.id,
+				'account_id': debit_account.id,
+				'currency_id': refund.currency_id.id,
+				'debit': 0,
+				'credit': line.amount
+				}
+			debit_credit_id = self.env['account.move.line'].create(vals_debit)
                 return None
 
 class refund_add_invoice_line(models.TransientModel):
