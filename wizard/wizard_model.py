014a6b35d226a4ae050fd293bbcb6e32850b9874
morrillo <gustavo.orrillo@moldeo.coop>
2016-07-10 14:01:49 -0300

    Final commit

--- wizard/wizard_model.py
+++ wizard/wizard_model.py
@@ -73,7 +73,9 @@ class refund_add_invoice(models.TransientModel):
 			move_id.post()
 		import pdb;pdb.set_trace()
 		for record_ids in rec_ids:
-			reconcile = self.env['account.move.line'].reconcile_partial(record_ids, writeoff_acc_id=None, writeoff_period_id=None, writeoff_journal_id=None)
+			#reconcile = self.env['account.move.line'].reconcile_partial(self.env.cr, self.env.uid, record_ids, \
+			#	type='auto', context=None, writeoff_acc_id=False, writeoff_period_id=False, writeoff_journal_id=False)
+			reconcile = self.env['account.move.line'].reconcile_partial(self.env.cr, self.env.uid,record_ids,'auto')
                 return None
 
 class refund_add_invoice_line(models.TransientModel):
